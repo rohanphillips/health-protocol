56a95958a20dd78263d1ce2cba6ae3b7e49a1aec
rohanphillips <rohan@solutions-insurance.com>
2019-09-16 15:30:54 -0500

    Add User Edit functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,19 +17,38 @@ class UsersController < ApplicationController
 
   # # GET: /users/5
   get "/users/:id" do
-    @user = User.find(params[:id])
-    erb :"/users/show.html"
+    if is_logged_in?
+      @user = User.find(params[:id])
+      erb :"/users/show.html"
+    else
+      redirect to ("/")
+    end
   end
 
   # # GET: /users/5/edit
-  # get "/users/:id/edit" do
-  #   erb :"/users/edit.html"
-  # end
+  get "/users/:id/edit" do
+    if is_logged_in?
+      @user = User.find(params[:id])
+      erb :"/users/edit.html"
+    else
+      redirect to "/"
+    end
+  end
 
   # # PATCH: /users/5
-  # patch "/users/:id" do
-  #   redirect "/users/:id"
-  # end
+  patch "/users/:id" do
+    if is_logged_in?
+      @user = User.find(params[:id])
+      if params[:user][:password] != ""
+        @user.update(params[:user])
+      else
+        @user.update(username: params[:user][:username], email: params[:user][:email])
+      end
+      redirect "/users/#{@user.id}"
+    else
+      redirect to ("/")
+    end
+  end
 
   # # DELETE: /users/5/delete
   # delete "/users/:id/delete" do
